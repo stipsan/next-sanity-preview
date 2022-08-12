c1a53fac3550fa8e17856852eb0bc8b58e0160fc
Cody Olsen <stipsan@gmail.com>
2022-08-12 16:47:24 +0200

    chore: compare bundlesize without preview mode

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,13 @@ import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { CMS_NAME } from '../lib/constants'
 import { indexQuery } from '../lib/queries'
-import { usePreviewSubscription } from '../lib/sanity'
-import { getClient, overlayDrafts } from '../lib/sanity.server'
+import { getClient } from '../lib/sanity.server'
 
-export default function Index({ allPosts: initialAllPosts, preview }) {
-  const { data: allPosts } = usePreviewSubscription(indexQuery, {
-    initialData: initialAllPosts,
-    enabled: preview,
-  })
+export default function Index({ allPosts }) {
   const [heroPost, ...morePosts] = allPosts || []
   return (
     <>
-      <Layout preview={preview}>
+      <Layout>
         <Head>
           <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
@@ -40,10 +35,10 @@ export default function Index({ allPosts: initialAllPosts, preview }) {
   )
 }
 
-export async function getStaticProps({ preview = false }) {
-  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery))
+export async function getStaticProps() {
+  const allPosts = await getClient(false).fetch(indexQuery)
   return {
-    props: { allPosts, preview },
+    props: { allPosts },
     // If webhooks isn't setup then attempt to re-generate in 1 minute intervals
     revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 60,
   }
