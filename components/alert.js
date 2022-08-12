172b413e94d016c54661ad6ed9de1d9ebadc1167
Cody Olsen <stipsan@gmail.com>
2022-08-12 17:47:27 +0200

    fix: use the new preview

--- components/alert.js
+++ components/alert.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Container from './container'
 import cn from 'classnames'
 import { EXAMPLE_PATH } from '../lib/constants'
@@ -11,16 +12,15 @@ export default function Alert({ preview }) {
       })}
     >
       <Container>
-        <div className="py-2 text-center text-sm">
+        <div className="py-2 text-sm text-center">
           {preview ? (
             <>
               This page is a preview.{' '}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-cyan duration-200 transition-colors"
-              >
-                Click here
-              </a>{' '}
+              <Link href="/api/exit-preview" prefetch={false}>
+                <a className="underline transition-colors duration-200 hover:text-cyan">
+                  Click here
+                </a>
+              </Link>{' '}
               to exit preview mode.
             </>
           ) : (
@@ -28,7 +28,7 @@ export default function Alert({ preview }) {
               The source code for this blog is{' '}
               <a
                 href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
+                className="underline transition-colors duration-200 hover:text-success"
               >
                 available on GitHub
               </a>
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,85 +1,107 @@
+import dynamic from 'next/dynamic'
+import ErrorPage from 'next/error'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
+import { useState } from 'react'
 import Container from '../../components/container'
-import PostBody from '../../components/post-body'
-import MoreStories from '../../components/more-stories'
 import Header from '../../components/header'
-import PostHeader from '../../components/post-header'
-import SectionSeparator from '../../components/section-separator'
 import Layout from '../../components/layout'
+import MoreStories from '../../components/more-stories'
+import PostBody from '../../components/post-body'
+import PostHeader from '../../components/post-header'
 import PostTitle from '../../components/post-title'
+import SectionSeparator from '../../components/section-separator'
 import { CMS_NAME } from '../../lib/constants'
 import { postQuery, postSlugsQuery } from '../../lib/queries'
-import { urlForImage, usePreviewSubscription } from '../../lib/sanity'
-import { sanityClient, getClient, overlayDrafts } from '../../lib/sanity.server'
+import { urlForImage } from '../../lib/sanity'
+import { getClient, overlayDrafts, sanityClient } from '../../lib/sanity.server'
+import { sanityConfig } from '../lib/config'
 
-export default function Post({ data = {}, preview }) {
+const PreviewMode = dynamic(() => import('next-sanity/preview'))
+
+export default function Post({ data: initialData = {}, preview }) {
   const router = useRouter()
+  const [data, setData] = useState(initialData)
 
   const slug = data?.post?.slug
-  const {
-    data: { post, morePosts },
-  } = usePreviewSubscription(postQuery, {
-    params: { slug },
-    initialData: data,
-    enabled: preview && slug,
-  })
+  const { post, morePosts } = data
 
   if (!router.isFallback && !slug) {
     return <ErrorPage statusCode={404} />
   }
 
   return (
-    <Layout preview={preview}>
-      <Container>
-        <Header />
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article>
-              <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
-                {post.coverImage?.asset?._ref && (
-                  <meta
-                    key="ogImage"
-                    property="og:image"
-                    content={urlForImage(post.coverImage)
-                      .width(1200)
-                      .height(627)
-                      .fit('crop')
-                      .url()}
-                  />
-                )}
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-            <SectionSeparator />
-            {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-          </>
-        )}
-      </Container>
-    </Layout>
+    <>
+      {preview && slug && (
+        <PreviewMode
+          projectId={sanityConfig.projectId}
+          dataset={sanityConfig.dataset}
+          initial={initialData}
+          query={postQuery}
+          onChange={setData}
+          token={token}
+          params={{ slug }}
+        />
+      )}
+      <Layout preview={preview}>
+        <Container>
+          <Header />
+          {router.isFallback ? (
+            <PostTitle>Loading…</PostTitle>
+          ) : (
+            <>
+              <article>
+                <Head>
+                  <title>
+                    {post.title} | Next.js Blog Example with {CMS_NAME}
+                  </title>
+                  {post.coverImage?.asset?._ref && (
+                    <meta
+                      key="ogImage"
+                      property="og:image"
+                      content={urlForImage(post.coverImage)
+                        .width(1200)
+                        .height(627)
+                        .fit('crop')
+                        .url()}
+                    />
+                  )}
+                </Head>
+                <PostHeader
+                  title={post.title}
+                  coverImage={post.coverImage}
+                  date={post.date}
+                  author={post.author}
+                />
+                <PostBody content={post.content} />
+              </article>
+              <SectionSeparator />
+              {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+            </>
+          )}
+        </Container>
+      </Layout>
+    </>
   )
 }
 
-export async function getStaticProps({ params, preview = false }) {
-  const { post, morePosts } = await getClient(preview).fetch(postQuery, {
+export async function getStaticProps({
+  params,
+  preview = false,
+  previewData = {},
+}) {
+  const client =
+    preview && previewData?.token
+      ? getClient(false).withConfig({ token: previewData.token })
+      : getClient(preview)
+  const { post, morePosts } = await client.fetch(postQuery, {
     slug: params.slug,
   })
 
   return {
     props: {
       preview,
+      token: (preview && previewData.token) || '',
       data: {
         post,
         morePosts: overlayDrafts(morePosts),
